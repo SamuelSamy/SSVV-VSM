38156bbaa0c11fc0f3f9e6926c4be8fd5e44e5e8
Samuel <fatu.samuel24@gmail.com>
2024-03-18 18:41:45 +0200

    Update StudentValidator.java

--- VreauSaMor/src/main/java/validation/StudentValidator.java
+++ VreauSaMor/src/main/java/validation/StudentValidator.java
@@ -14,21 +14,27 @@ public class StudentValidator implements Validator<Student> {
         if(entity.getID() == null){
             throw new ValidationException("Id incorect!");
         }
+
         if(entity.getID().equals("")){
             throw new ValidationException("Id incorect!");
         }
-        if(entity.getNume() == ""){
-            throw new ValidationException("Nume incorect!");
-        }
-        if(entity.getGrupa() < 0) {
-            throw new ValidationException("Grupa incorecta!");
-        }
-        if(entity.getEmail() == null){
-            throw new ValidationException("Email incorect!");
-        }
+
         if(entity.getNume() == null){
             throw new ValidationException("Nume incorect!");
         }
+
+        if(entity.getNume() == ""){
+            throw new ValidationException("Nume incorect!");
+        }
+
+        if(entity.getGrupa() < 0) {
+            throw new ValidationException("Grupa incorecta!");
+        }
+
+        if(entity.getEmail() == null){
+            throw new ValidationException("Email incorect!");
+        }
+
         if(entity.getEmail().equals("")){
             throw new ValidationException("Email incorect!");
         }
